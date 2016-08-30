7b2d5c7b742a69992e2fcff1cde1e97811a3d93a
sarahjer <sweetiejeremy@hotmail.com>
2016-08-30 15:15:06 -0700

    Update binarysearchtree.js

--- Exercises/binarysearchtree.js
+++ Exercises/binarysearchtree.js
@@ -3,35 +3,35 @@ function BinarySearchTree {
 	BinarySearchTree.prototype {
 		add: function(value) {
 		var current = this._root;
-    var node = {
-				value: value,
-				left: null,
-				right: null
+                var node = {
+	            value: value,
+		    left: null,
+		    right: null
 		};
 		if (this._root === null) {
-				this._root = node;
+		    this._root = node;
 		} else {
-				current = this._root;
-				while (true) {
-				  if (value < current.value) {
-					  	if (current === null) {
-								  current.left = node;
-								  break;
-							} else {
-									current=current.left;
-							}
-					} else if (value > current.value) {
-								 if (current === null) {
-										 current.right=node;
-										 break;
-									} else {
-											current = current.right;
-									}
-						      } else {
-							       break;
-					        }
-				  }
-		  }
-	 },
+		    current = this._root;
+		    while (true) {
+			if (value < current.value) {
+				if (current === null) {
+				    current.left = node;
+				    break;
+				} else {
+				     current=current.left;
+				}
+			} else if (value > current.value) {
+				if (current === null) {
+					current.right=node;
+					break;
+				} else {
+				    current = current.right;
+				}
+			} else {
+			    break;
+			}
+			}
+		}
+	},
 	}
 };
