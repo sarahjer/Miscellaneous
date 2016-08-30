220cc7374de645fa60ba6fd167b45cce705c5b4f
Sarah <sweetiejeremy@hotmail.com>
2016-08-30 16:21:02 -0700

    Improved Indentations

--- Exercises/binarysearchtree.js
+++ Exercises/binarysearchtree.js
@@ -1,35 +1,37 @@
 function BinarySearchTree {
-	this._root=null;
-	BinarySearchTree.prototype {
-		add: function(value) {
-			var current = this._root;
-			var node = {
-				value: value,
-				left: null,
-				right: null
-			};
-			if (this._root === null) {
-				this._root = node;
-			} else {
-				current = this._root;
-				while (true) {
-					if (value < current.value) {
-						if (current === null) {
-							current.left = node;
-							break;
+		this._root=null;
+		BinarySearchTree.prototype {
+				add: function(value) {
+						var current = this._root;
+						var node = {
+								value: value,
+								left: null,
+								right: null
+						};
+						if (this._root === null) {
+								this._root = node;
 						} else {
-							current = current.left;
+								current = this._root;
+								while (true) {
+										if (value < current.value) {
+												if (current === null) {
+														current.left = node;
+														break;
+													} else {
+															current = current.left;
+													}
+											} else if (value > current.value) {
+													if (current === null) {
+															current.right = node;
+															break;
+														} else {
+																	current = current.right;
+														}
+											} else {
+													break;
+											}
+								}
 						}
-					} else if (value > current.value) {
-						if (current === null) {
-							current.right = node;
-							break;
-						} else {
-							current = current.right;
-						}
-					} else {
-						break;
-					}
-				}
-			}
-		},
+				},
+		}
+};
