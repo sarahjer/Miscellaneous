447cd79f00705691032b168bd0833f651e8ab31d
Sarah <sweetiejeremy@hotmail.com>
2016-08-31 11:59:43 -0700

    Improved Indentations

--- Exercises/binarysearchtree.js
+++ Exercises/binarysearchtree.js
@@ -1,38 +1,41 @@
 function BinarySearchTree() {
-		this._root = null;
-		BinarySearchTree.prototype = {
-				add: function(value) {
-						var node = {
-								value: value,
-								left: null,
-								right: null
-						},
-						current;
-						if (this._root === null) {
-								this._root = node;
+	this._root = null;
+	BinarySearchTree.prototype = {
+	  constructor: BinarySearchTree,
+		add: function(value) {
+			var node = {
+				value: value,
+				left: null,
+				right: null
+			},
+			current;
+			if (this._root === null) {
+				this._root = node;
+			} else {
+				current = this._root;
+				while (true) {
+					if (value < current.value) {
+						if (current.left === null) {
+							current.left = node;
+							break;
 						} else {
-							  current = this._root;
-								while (true) {
-										if (value < current.value) {
-												if (current.left === null) {
-														current.left = node;
-														break;
-													} else {
-															current = current.left;
-													}
-											} else if (value > current.value) {
-													if (current.right === null) {
-															current.right = node;
-															break;
-														} else {
-																	current = current.right;
-														}
-											} else {
-													break;
-											}
-								}
+							current = current.left;
 						}
-				},
-		}
+					} else if (value > current.value) {
+						if (current.right === null) {
+							current.right = node;
+							break;
+						} else {
+							current = current.right;
+						}
+					} else {
+						break;
+					}
+				}
+			}
+		},
+	}
 };
-BinarySearchTree ();
+BinarySearchTree();
+BinarySearchTree.prototype.add(4);
+console.log(node);
