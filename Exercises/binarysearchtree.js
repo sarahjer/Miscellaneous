a0ad87a0409516c3e08941420712a10d31f13932
Sarah <sweetiejeremy@hotmail.com>
2016-08-30 17:18:08 -0700

    Fixed Bugs

--- Exercises/binarysearchtree.js
+++ Exercises/binarysearchtree.js
@@ -1,27 +1,27 @@
 function BinarySearchTree() {
-		this._root=null;
+		this._root = null;
 		BinarySearchTree.prototype = {
 				add: function(value) {
-						var current = this._root;
 						var node = {
 								value: value,
 								left: null,
 								right: null
-						};
+						},
+						current;
 						if (this._root === null) {
 								this._root = node;
 						} else {
-								current = this._root;
+							  current = this._root;
 								while (true) {
 										if (value < current.value) {
-												if (current === null) {
+												if (current.left === null) {
 														current.left = node;
 														break;
 													} else {
 															current = current.left;
 													}
 											} else if (value > current.value) {
-													if (current === null) {
+													if (current.right === null) {
 															current.right = node;
 															break;
 														} else {
