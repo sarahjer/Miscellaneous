65aca8807b74568e356a8ac221d6c2eb68b7fd0d
Sarah <sweetiejeremy@hotmail.com>
2016-09-13 16:26:26 -0700

    Added Break Statement

--- Exercises/Project Euler/LargestPalindromeProduct/Script.js
+++ Exercises/Project Euler/LargestPalindromeProduct/Script.js
@@ -1,13 +1,16 @@
 function Palindrome() {
 	for (var i = 999; i >= 100; i--){
 		for(var j = 999; j >= 100; j--){
-			var a = i * j;
+			var a = i * j, c;
 			b = a.toString();
 			if (b == b.split('').reverse().join('')) {
-			console.log (i + "x" + j);
-			break;
+			c = i + "x" + j;
+			console.log(c);
 			}
-		}	
+			if (c != undefined) {
+				break;
+			}
+		}
 	}
 }
 
